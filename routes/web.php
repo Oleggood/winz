790d185f1f7a042f4f7a5b65e6ab823df8ed531d
Oleggood <oleggood@mail.ru>
2024-03-15 16:48:49 +0300

    todo routs

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,56 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Back;
+use App\Http\Controllers\Front;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+//use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Pluralizer;
+
+Route::get('/{locale}', function (string $locale) {
+    if (! in_array($locale, ['en', 'ru',])) {
+        abort(400);
+    }
+
+    $xxx = App::setLocale($locale);
+    dd($xxx);
+    //так установить префикс!
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+
+//были после развертывания пустого проекта laravel:
+
+//use App\Http\Controllers\ProfileController;
+//use Illuminate\Foundation\Application;
+//use Illuminate\Support\Facades\Route;
+//use Inertia\Inertia;
+
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//});
+//
+//
+//Route::get('/dashboard', function () {
+//    return Inertia::render('Dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+//
+//Route::middleware('auth')->group(function () {
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
+
+require __DIR__ . '/auth.php';
